5dfbb5c29ee02bd9345c0c46e97b75df718b6009
Christophe Bury <christophe.bury@braincube.com>
2025-02-18 14:11:26 +0100

    Add ttkbootstrap project

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as ttk
+
+root = ttk.Window(themename="superhero")
+
+root.title('MIDI Clock')
+root.geometry('500x350')
+root.mainloop()
