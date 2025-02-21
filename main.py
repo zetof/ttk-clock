873cce195b2697f33f04120504bd810327987849
Christophe Bury <christophe.bury@braincube.com>
2025-02-21 11:19:08 +0100

    Pack everything, great mess

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ window.geometry('300x150')
 
 # Create frame layout
 top_frame = Frame(window, bg='cyan')
-top_left_frame = Frame(top_frame, bg='cyan')
+top_right_frame = Frame(top_frame, bg='green')
 middle_frame = Frame(window, bg='blue')
 bottom_frame = Frame(window, bg='yellow')
 
@@ -32,10 +32,20 @@ def send():
 # Create widgets
 live_radio = Radiobutton(top_frame, variable=is_live, text='LIVE', value=True, command=change_mode)
 onsend_radio = Radiobutton(top_frame, variable=is_live, text='ONSEND', value=False, command=change_mode)
-send_button = Button(top_left_frame, text='SEND', command=send)
+send_button = Button(top_right_frame, text='SEND', command=send)
 play_pause_button = Button(middle_frame, text='PLAY', command=play_pause)
 rewind_button = Button(middle_frame, text='REWIND', command=rewind)
 bpm_slider = Scale(bottom_frame, from_=40, to=300, orient=HORIZONTAL)
 
-
+# Pack things
+top_frame.pack()
+live_radio.pack()
+onsend_radio.pack()
+top_right_frame.pack()
+send_button.pack()
+middle_frame.pack()
+play_pause_button.pack()
+rewind_button.pack()
+bottom_frame.pack()
+bpm_slider.pack()
 window.mainloop()
