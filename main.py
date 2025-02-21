c8a4f6d5bd4f82c9e7fe6bc86fb83e66bd861697
Christophe Bury <christophe.bury@braincube.com>
2025-02-21 10:59:05 +0100

    Revert to plain olf Tkinter and set base layouts and widgets

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from tkinter import *
-from ttkbootstrap.constants import *
-import ttkbootstrap as ttk
+from tkinter import ttk
 
-root = ttk.Window(themename="superhero")
+# Create top window
+window = Tk()
+window.title('MIDI CLOCK')
+window.geometry('300x150')
 
-root.title('MIDI Clock')
-root.geometry('500x350')
-root.mainloop()
+# Create frame layout
+top_frame = Frame(window, bg='cyan')
+top_left_frame = Frame(top_frame, bg='cyan')
+middle_frame = Frame(window, bg='blue')
+bottom_frame = Frame(window, bg='yellow')
+
+# Event variables
+is_live = BooleanVar(value=True)
+is_playing = BooleanVar(value=False)
+
+# Event functions
+def change_mode():
+  pass
+
+def play_pause():
+  pass
+
+def rewind():
+  pass
+
+def send():
+  pass
+
+# Create widgets
+live_radio = Radiobutton(top_frame, variable=is_live, text='LIVE', value=True, command=change_mode)
+onsend_radio = Radiobutton(top_frame, variable=is_live, text='ONSEND', value=False, command=change_mode)
+send_button = Button(top_left_frame, text='SEND', command=send)
+play_pause_button = Button(middle_frame, text='PLAY', command=play_pause)
+rewind_button = Button(middle_frame, text='REWIND', command=rewind)
+bpm_slider = Scale(bottom_frame, from_=40, to=300, orient=HORIZONTAL)
+
+
+window.mainloop()
