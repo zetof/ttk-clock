da403dc415e178bdf55d25574e8695be35506d8b
ZeToF! <zetof@zetof.net>
2025-02-24 19:29:59 +0100

    More or less ok layout

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from tkinter import ttk
 # Create top window
 window = Tk()
 window.title('MIDI CLOCK')
-window.geometry('300x150')
 
 # Create frame layout
-top_frame = Frame(window, bg='cyan')
-top_left_frame = Frame(top_frame, bg='green')
-middle_frame = Frame(window, bg='blue')
-bottom_frame = Frame(window, bg='yellow')
+top_frame = Frame(window)
+middle_frame = Frame(window)
+bottom_frame = Frame(window)
 
 # Event variables
 is_live = BooleanVar(value=True)
@@ -30,22 +28,25 @@ def send():
   pass
 
 # Create widgets
-live_radio = Radiobutton(top_left_frame, variable=is_live, text='LIVE', value=True, command=change_mode)
-onsend_radio = Radiobutton(top_left_frame, variable=is_live, text='ONSEND', value=False, command=change_mode)
+live_radio = Radiobutton(top_frame, variable=is_live, text='LIVE', value=True, command=change_mode)
+onsend_radio = Radiobutton(top_frame, variable=is_live, text='ONSEND', value=False, command=change_mode)
 send_button = Button(top_frame, text='SEND', command=send)
 play_pause_button = Button(middle_frame, text='PLAY', command=play_pause)
 rewind_button = Button(middle_frame, text='REWIND', command=rewind)
 bpm_slider = Scale(bottom_frame, from_=40, to=300, orient=HORIZONTAL)
 
 # Pack things
-top_frame.pack()
-top_left_frame.pack(side="left")
-live_radio.pack()
-onsend_radio.pack()
-send_button.pack()
-middle_frame.pack()
-play_pause_button.pack(side="left")
-rewind_button.pack(side="left")
-bottom_frame.pack()
-bpm_slider.pack()
+top_frame.pack(fill=X, padx=5, pady=2)
+top_frame.columnconfigure(0, weight=1)
+top_frame.columnconfigure(1, weight=1)
+live_radio.grid(row=0, column=0, padx=10, pady=(5, 0), sticky='w')
+onsend_radio.grid(row=1, column=0, padx=10, pady=5, sticky='w')
+send_button.grid(row=0, column=1, rowspan=2, padx=10, sticky='e')
+middle_frame.pack(fill=X, padx=5, pady=2)
+middle_frame.columnconfigure(0, weight=1)
+middle_frame.columnconfigure(1, weight=1)
+play_pause_button.grid(row=0, column=0, padx=10, sticky='w')
+rewind_button.grid(row=0, column=1, padx=10, sticky='e')
+bottom_frame.pack(fill=X,padx=5, pady=2)
+bpm_slider.pack(fill=X, padx=10)
 window.mainloop()
